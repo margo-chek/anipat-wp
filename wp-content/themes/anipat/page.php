17fa7958ff7869d7c1e6ba066d5e5a74941eb4e2
Margo <mnchek@mail.ru>
2020-06-04 00:55:15 +0300

    fix home page

--- wp-content/themes/anipat/page.php
+++ wp-content/themes/anipat/page.php
@@ -12,27 +12,24 @@
  * @package Anipat
  */
 
-get_header();
-?>
+	while ( have_posts() ) :
+		the_post();
 
-	<main id="primary" class="site-main">
+		$id = get_the_ID();
+		if ( $id == 25) {
+			wp_redirect( home_url() );
+			exit;
+		}
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		get_header();
 
-			get_template_part( 'template-parts/content', 'page' );
+		get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
 
-		endwhile; // End of the loop.
-		?>
+	endwhile; // End of the loop.
 
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
 get_footer();
